ce69c5359422867c9230602d5b55f2f1e1d7bd7d
gamearoo10 <47373781+gamearoo10@users.noreply.github.com>
2019-04-07 17:19:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const bot = new discord.Client;
 
 bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
-    bot.user.setGame("!help | Bot Made By: Yourname")
+    bot.user.setGame("m.help | Bot Made By: Gamearoo#0001")
 });
 
 bot.on("message", async message => {
@@ -18,14 +18,33 @@ bot.on("message", async message => {
     let cmd = messageArry[0];
     let args = messageArry.slice(1);
 
-    if(cmd === `${prefix}hello`){
-       return message.channel.send("Hello");
+    if(cmd === `${prefix}botinfo`){
+
+    let bicon = bot.user.avatarURL;
+
+    let botembed = new discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#3e15f2") //can any hex code
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Created On", bot.user.createdAt);
+
+    return message.channel.send(botembed);
     }
 
-    
-    
+    if(cmd === `${prefix}serverinfo`){
+        let sicon = message.guild.iconURL;
+        let serverenbed = new discord.RichEmbed()
+        .setDescription("Server Information")
+        .setColor("#3e15f2") //can be any hex code
+        .setThumbnail(sicon)
+        .addField("Server Name", message.guild.name)
+        .addField("Created On", message.guild.createdAt)
+        .addField("You Joined", message.member.joinedAt)
+        .addField("Total Members", message.guild.memberCount);
 
-    
+        return message.channel.send(serverenbed);
+    }
 });
 
 bot.login(config.token);
